7c8db65a352ba5f326749a658a1858a866c07741
saaroca <saaroca@gmail.com>
2023-03-23 08:23:17 +0100

    Proyecto finalizado

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,13 @@
+import '../styles/Spinner.css'
+
+const Spinner = () => {
+  return (
+    <div>
+        <div className="spinner">
+        <div className="double-bounce1"></div>
+        <div className="double-bounce2"></div>
+  </div></div>
+  )
+}
+
+export default Spinner
